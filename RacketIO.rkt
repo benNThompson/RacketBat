40daa4066c002b2910352d0a25b2568fe952e3ec
Ben Thompson <ben.n.thompson@gmail.com>
2010-10-26 08:48:24 -0400

    Added the basic Racket IO

--- RacketIO.rkt
+++ RacketIO.rkt
@@ -1,2 +1,23 @@
 #lang racket
 (require racket/tcp)
+
+;;OUT
+
+(define-values (in out) (tcp-connect "localhost" 2000))
+
+(display "Hello there\n" out)
+
+(close-output-port out)
+(close-input-port in)
+
+;;IN
+
+(define lstnr (tcp-listen 2000))
+(define-values (in out) (tcp-accept lstnr))
+
+(for ([line (in-lines in)])
+  (display line))
+
+(close-input-port in)
+(close-output-port out)
+(tcp-close lstnr)
