d8878ef6f921305e4431e8110d55747888e0196d
Ben Thompson <ben.n.thompson@gmail.com>
2010-10-28 09:15:28 -0400

    Trying to get back and forth between racket and java

--- RacketIO.rkt
+++ RacketIO.rkt
@@ -3,21 +3,25 @@
 
 ;;OUT
 
-;(define-values (in out) (tcp-connect "localhost" 2000))
+(define-values (in out) (tcp-connect "localhost" 2000))
 
-;(display "Hello there\n" out)
+(display "Hello there\n" out)
 
-;(close-output-port out)
-;(close-input-port in)
+(close-output-port out)
+(close-input-port in)
 
 ;;IN
 
-(define lstnr (tcp-listen 2000))
-(define-values (in out) (tcp-accept lstnr))
-
-(for ([line (in-lines in)])
-  (display line))
-
-(close-input-port in)
-(close-output-port out)
-(tcp-close lstnr)
+;(define lstnr (tcp-listen 2000))
+;(define-values (in out) (tcp-accept lstnr))
+;
+;(for ([line (in-lines in)])
+;  (display (string-append line "\n")))
+;
+;;(display "Yep," out)
+;;(display "it worked." out)
+;;(display "Huzzah!" out)
+;
+;(close-input-port in)
+;(close-output-port out)
+;(tcp-close lstnr)
