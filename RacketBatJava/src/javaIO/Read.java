018fb189730c46cb89556588df295e473ed7819e
Ben Thompson <ben.n.thompson@gmail.com>
2010-11-04 08:10:51 -0400

    FINALLY GOT IO WORKING!!!!!!

--- RacketBatJava/src/javaIO/Read.java
+++ RacketBatJava/src/javaIO/Read.java
@@ -7,35 +7,38 @@ import java.net.Socket;
 import java.net.ServerSocket;
 
 public class Read {
-//	public static void main(String[] args) throws Exception {
-//		ServerSocket ss = new ServerSocket(2000);
-//		Socket sock = ss.accept();
-//		BufferedReader in = new BufferedReader(
-//			new InputStreamReader(sock.getInputStream()));
-//		String input;
-//
-//		while ((input = in.readLine()) != null) {
-//			System.out.println(input);
-//		}
-//		in.close();
-//		sock.close();
-//		ss.close();
-//	}
 
 	public static void main(String[] args) throws Exception {
+		out();
+		in();
+	}
+	
+	public static void out() throws Exception {
 		Socket sock = new Socket("localhost", 2000);
 		PrintWriter out = new PrintWriter(sock.getOutputStream());
 		out.println("It worked.");
 		out.println("At least,");
 		out.println("we think it did.");
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(sock.getInputStream()));
-		String input;
-		while ((input = in.readLine()) != null) {
-				System.out.println(input);
-		}
-		in.close();
 		out.close();
 		sock.close();
 	}
+	
+	public static void in() throws Exception {
+		ServerSocket ss = new ServerSocket(2001);
+		Socket sock = ss.accept();
+		BufferedReader in = new BufferedReader(
+			new InputStreamReader(sock.getInputStream()));
+		String input;
+
+		while ((input = in.readLine()) != null) {
+			System.out.println(input);
+		}
+		in.close();
+		sock.close();
+		ss.close();
+	}
 }
+
+
+
+
--- RacketIO.rkt
+++ RacketIO.rkt
@@ -1,16 +1,21 @@
 #lang racket
 (require racket/tcp)
 
-;;;OUT
-;
-;(define-values (in out) (tcp-connect "localhost" 2000))
-
-
+;In
 (define lstnr (tcp-listen 2000))
 (define-values (in out) (tcp-accept lstnr))
 
 (for ([line (in-lines in)])
   (display (string-append line "\n")))
+
+(close-input-port in)
+(close-output-port out)
+(tcp-close lstnr)
+
+;Out
+(sleep .5)
+(set!-values (in out)
+             (tcp-connect "localhost" 2001))
 
 (display "Yep," out)
 (display "it worked." out)
@@ -18,4 +23,3 @@
 
 (close-input-port in)
 (close-output-port out)
-(tcp-close lstnr)
