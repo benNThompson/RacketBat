1d7d55bdff1a9fa4545ef672f0a488ae51a5a5d5
Ben Thompson <ben.n.thompson@gmail.com>
2010-11-17 09:04:39 -0500

    Got the grader working for one check at a time

--- Grader/Assignment.rkt
+++ Grader/Assignment.rkt
@@ -4,8 +4,11 @@
 
 (define ev (make-module-evaluator (string->path "StudentWork.rkt")
                                   #:allow-read (list (string->path "."))))
+
 (define my-add-tests (check-suite
-                      ([(my-add 1 2) 3 1]
-                       [(my-add 3 7) 10 1]
-                       [(my-add 10 -5) 5 2]
-                       [(my-add 1 0) 3 10])))
+                      ((my-add 1 2) 3 1)
+                      ((my-add 3 7) 10 1)
+                      ((my-add 10 -5) 5 2)
+                      ((my-add 1 0) 3 10)))
+
+(run-checks my-add-tests ev)
--- Grader/Grader.rkt
+++ Grader/Grader.rkt
@@ -14,27 +14,34 @@
 ;; produces: nothing, but defines aName as a list of the checks
 (define-syntax check-suite
   (syntax-rules ()
-    ((_ ([actualResult expectedResult pointValue] ...))
+    ((_ (actualResult expectedResult pointValue) ...)
      (list (check (quote actualResult) expectedResult pointValue) ...))))
 
 ;; run-checks: list-of-checks evaluator -> void
 ;; consumes: a list of checks and an evaluator to run them against
 ;; produces: Displays the number passed and failed, and the points scored out of the possible points
-(define (run-checks aSuite anEva)
-  (let ([totalPoints 0]
-        [numChecks (length aSuite)]
-        [points 0]
-        [passed 0])
-    (for ([c aSuite])
-      (set! totalPoints (+ totalPoints (check-points c)))
-      (when (equal? (anEva (check-actual c)) (anEva (check-expected c)))
-          (set! points (+ points (check-points c)))
-          (set! passed (+ passed 1))))
-    (display (string-append "Passed/Failed: "
-                            (number->string passed)
-                            "/"
-                            (number->string numChecks)
-                            "\nPoints: "
-                            (number->string points)
-                            "/"
-                            (number->string totalPoints)))))
+(define-syntax run-checks
+  (syntax-rules ()
+    ((_ aSuite anEva)
+     (let ([totalPoints 0]
+           [numChecks (length aSuite)]
+           [points 0]
+           [passed 0]
+           [name (symbol->string (quote aSuite))])
+       (for ([c aSuite])
+         (set! totalPoints (+ totalPoints (check-points c)))
+         (when (equal? (anEva (check-actual c)) (anEva (check-expected c)))
+           (set! points (+ points (check-points c)))
+           (set! passed (+ passed 1))))
+       (display (string-append name
+                               "\n"
+                               (build-string (string-length name) (lambda (i) #\-))
+                               "\n"
+                               "Passed/Failed: "
+                               (number->string passed)
+                               "/"
+                               (number->string numChecks)
+                               "\nPoints: "
+                               (number->string points)
+                               "/"
+                               (number->string totalPoints)))))))
